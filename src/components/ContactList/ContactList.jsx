6cb1aa72ac31813b1aa800d86bdd702a558b4003
Roman <xoxorich@proton.me>
2024-10-04 15:54:22 +0300

    finish

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+import { selectContacts } from "../../redux/contactsSlice";
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+
+  return (
+    <ul className={s.list}>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <Contact id={id} name={name} number={number} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
